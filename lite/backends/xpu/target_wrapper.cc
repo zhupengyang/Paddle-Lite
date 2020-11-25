1628227e358d5118ae7f482d5cc11b534fc8c4a5
root <root@gzbh-ps-www170-27cee.gzbh.baidu.com>
2020-11-25 12:12:38 +0000

    lock-l3

--- lite/backends/xpu/target_wrapper.cc
+++ lite/backends/xpu/target_wrapper.cc
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "lite/backends/xpu/target_wrapper.h"
+#include <fcntl.h>
 #include "lite/utils/macros.h"
 
 namespace paddle {
@@ -75,9 +76,45 @@ XPUScratchPadGuard TargetWrapperXPU::MallocScratchPad(size_t size,
   return XPUScratchPadGuard(new XPUScratchPad(ptr, size, use_l3));
 }
 
+void TargetWrapperXPU::LockXPU(int dev_no) {
+  // TODO(zhupengyang): support multi-xpu later
+  // lock thread mutex
+  mutex_.lock();
+
+  // lock process mutex
+  struct flock f_lock;
+  f_lock.l_whence = 0;
+  f_lock.l_len = 0;
+  f_lock.l_type = F_WRLCK;
+
+  std::string buf = "/opt/xpu_lock" + std::to_string(dev_no);
+  CHECK_EQ(xpu_lock_fd_, -1) << "file: " << buf << " has been opened before.";
+  xpu_lock_fd_ = open(buf.c_str(), O_WRONLY | O_CREAT, S_IROTH | S_IWOTH);
+  CHECK_GT(xpu_lock_fd_, -1) << "open " << buf << " failed: " << xpu_lock_fd_;
+
+  fcntl(xpu_lock_fd_, F_SETLKW, &f_lock);
+}
+
+void TargetWrapperXPU::UnlockXPU(int dev_no) {
+  // unlock process mutex
+  struct flock f_lock;
+  f_lock.l_whence = 0;
+  f_lock.l_len = 0;
+  f_lock.l_type = F_UNLCK;
+  CHECK_GT(xpu_lock_fd_, -1) << "File may be closed before.";
+  fcntl(xpu_lock_fd_, F_SETLKW, &f_lock);
+  close(xpu_lock_fd_);
+  xpu_lock_fd_ = -1;
+
+  // unlock thread mutex
+  mutex_.unlock();
+}
+
 std::string TargetWrapperXPU::multi_encoder_precision;  // NOLINT
 int TargetWrapperXPU::workspace_l3_size_per_thread{0};
 LITE_THREAD_LOCAL xdnn::Context* TargetWrapperXPU::tls_raw_ctx_{nullptr};
+int TargetWrapperXPU::xpu_lock_fd_{-1};
+std::mutex TargetWrapperXPU::mutex_{};
 
 }  // namespace lite
 }  // namespace paddle
