098f5f941c9d23a432f6ea717695a78dc22de33c
zhupengyang <zhu_py@qq.com>
2021-03-24 06:43:44 +0000

    test

--- lite/core/mir/generate_program_pass.h
+++ lite/core/mir/generate_program_pass.h
@@ -14,12 +14,16 @@
 
 #pragma once
 
+#include <algorithm>
 #include <list>
 #include <memory>
+#include <string>
 #include <utility>
 #include <vector>
 #include "lite/core/kernel.h"
 #include "lite/core/mir/pass.h"
+#include "lite/kernels/host/conditional_block_compute.h"
+#include "lite/kernels/host/while_compute.h"
 
 namespace paddle {
 namespace lite {
@@ -31,10 +35,41 @@ namespace mir {
  */
 class GenerateProgramPass : public ProgramPass {
  public:
-  void Apply(const std::unique_ptr<SSAGraph> &graph) override;
+  void Apply(const std::unique_ptr<SSAGraph>& graph) override;
 
   std::unique_ptr<RuntimeProgram> GenProgram() {
     LOG(INFO) << "insts.size " << insts_.size();
+
+    // sub pragram
+    std::string sub_block_ops{"while", "conditional_block"};
+    for (auto& inst : insts_[0]) {
+      std::string op_name = inst.op->Type();
+      if (std::find(sub_block_ops.begin(), sub_block_ops.end(), op_name) ==
+          sub_block_ops.end()) {
+        continue;
+      }
+      CHECK(inst.op->op_info()->HasAttr("sub_block"));
+      int block_idx = inst.op()->op_info()->GetAttr<int>("sub_block");
+
+      if (op_name == "while") {
+        std::vector<std::vector<Instruction>> sub_insts{insts_[block_idx]};
+        std::unique_ptr<RuntimeProgram> sub_program(
+            new RuntimeProgram(std::move(sub_insts)));
+        auto* kernel =
+            static_cast<kernels::host::WhileCompute*>(inst.mutable_kernel());
+        kernel->SetRuntimeProgram(&sub_program);
+      }
+      if (op_name == "conditional_block") {
+        std::vector<std::vector<Instruction>> sub_insts{insts_[block_idx]};
+        std::unique_ptr<RuntimeProgram> sub_program(
+            new RuntimeProgram(std::move(sub_insts)));
+        auto* kernel = static_cast<kernels::host::ConditionalBlockCompute*>(
+            inst.mutable_kernel());
+        kernel->SetRuntimeProgram(&sub_program);
+      }
+    }
+
+    // main program
     std::unique_ptr<RuntimeProgram> program(
         new RuntimeProgram(std::move(insts_)));
 
